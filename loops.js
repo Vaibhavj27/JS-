e59e34f243de929220ccbc5bacd92b3e4716d825
vj9766876963 <vj9766876963@gmail.com>
2024-08-01 06:56:35 +0000

    loops syntax

---
+++ loops.js
@@ -0,0 +1,37 @@
+// date: 31/7/2024
+// for loop
+
+for(let i = 0; i <= 10; i++){
+    console.log(i);
+}
+
+// while loop
+let i=0;
+while(i<=10){
+    console.log(i);
+    i++;
+}
+
+// do while loop
+let j=0;
+do{
+    console.log(j);
+    j++;
+}while(j<=10);
+//**************ARRAY SPECIFIC LOOPS *************/
+//for of 
+const arr = [1,2,3,4,5]
+for (const i of arr) {
+    console.log(i);
+}
+//for of can be used to iterate through an string also
+const Name= "Vaibhav"
+
+
+for(const i of Name)
+    {
+        console.log(`each char is ${i}`);
+    }
+
+
+// maps
